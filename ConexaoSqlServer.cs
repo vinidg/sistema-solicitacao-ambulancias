4b9bc8242781dd14eeb89fab9643729182b9924c
vinidg <vinidgdon@gmail.com>
2015-09-04 11:15:17 -0300

    conexao

--- ConexaoSqlServer.cs
+++ ConexaoSqlServer.cs
@@ -14,9 +14,6 @@ namespace Solicitacao_de_Ambulancias
         public static SqlConnection GetConexao()
         {
 
-            string strCon = "data source = srv-mssql-01;initial catalog = DAHUE;user id = dahue; password = DT1_D@huE_1438_DtI";
-            // string strCon = "data source = dti-webhmg-01;initial catalog = DAHUE;user id = dahue; password = DT1_HmG_2005_DtI";
-            //string strCon = "data source = .\\SQLEXPRESS;initial catalog = DAHUE;user id = sa; password = 123456";
 
             SqlConnection conexao = new SqlConnection(strCon);
             conexao.Open();
